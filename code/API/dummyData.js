98bad07ba8ed889e92529e2efb34456ad405c2de
Xyntlock <70027309+Xyntlock@users.noreply.github.com>
2023-11-07 09:54:30 +0000

    Add dummy data GET endpoint
    Adds User GET endpoint that returns some dummy data about users

---
+++ code/API/dummyData.js
@@ -0,0 +1,36 @@
+const data = [
+    {
+        "id" : 1,
+        "name" : "James",
+        "journals" : [
+            {
+                "id" : 5,
+                "text" : "I had a bad day.",
+                "mood" : "sad"
+            },
+            {
+                "id" : 6,
+                "text" : "I had a good day.",
+                "mood" : "happy"
+            }
+        ]
+    },
+    {
+        "id" : 2,
+        "name" : "Tina",
+        "journals" : [
+            {
+                "id" : 7,
+                "text" : "I fell in love today!",
+                "mood" : "in love"
+            },
+            {
+                "id" : 8,
+                "text" : "We broke up :(",
+                "mood" : "heartbroken"
+            }
+        ]
+    }
+]
+
+export default data
