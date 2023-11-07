cc05a94b4e171545e85e138a243fd22123425d09
Emilijus Miniauskas <psyem1@nottingham.ac.uk>
2023-11-07 09:41:12 +0000

    Add sonarcloud to repo
    Add build.yaml to github workflows and update the gradle build file.

--- code/MindScribe/build.gradle.kts
+++ code/MindScribe/build.gradle.kts
@@ -1,4 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.1.2" apply false
+    id("org.sonarqube") version "4.4.1.3373"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "EmKaaaM_MindScribe")
+        property("sonar.organization", "team13")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
