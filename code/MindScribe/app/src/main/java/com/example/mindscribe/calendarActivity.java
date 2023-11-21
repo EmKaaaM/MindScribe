851e7d926a0a78f334a2f2aea35e33a4d4469807
Louismaurice11 <louismaurice11@hotmail.com>
2023-11-21 10:43:13 +0000

    Create layout for calendar activity

--- code/MindScribe/app/src/main/java/com/example/mindscribe/calendarActivity.java
+++ code/MindScribe/app/src/main/java/com/example/mindscribe/calendarActivity.java
@@ -3,12 +3,13 @@ package com.example.mindscribe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
 
-public class calenderActivity extends AppCompatActivity {
-
+public class calendarActivity extends AppCompatActivity {
+    CalendarView calenderView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calender);
+        setContentView(R.layout.activity_calendar);
     }
 }
