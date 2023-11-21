250b5d185737f8419d9161da6fb0bed5fd7f50e0
Louismaurice11 <louismaurice11@hotmail.com>
2023-11-21 10:53:37 +0000

    Finish calendar activity layout

--- code/MindScribe/app/src/main/java/com/example/mindscribe/MainActivity.java
+++ code/MindScribe/app/src/main/java/com/example/mindscribe/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.mindscribe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void calBtnOnClick(View v){
+        Intent intent = new Intent (MainActivity.this, CalendarActivity.class);
+        startActivity(intent);
     }
 }
