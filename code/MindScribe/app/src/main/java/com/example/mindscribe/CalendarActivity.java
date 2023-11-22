908c62cd93432f07f26c190401119b8facb6e05b
Louismaurice11 <louismaurice11@hotmail.com>
2023-11-22 13:13:53 +0000

    Add comments

--- code/MindScribe/app/src/main/java/com/example/mindscribe/CalendarActivity.java
+++ code/MindScribe/app/src/main/java/com/example/mindscribe/CalendarActivity.java
@@ -14,24 +14,23 @@ public class CalendarActivity extends AppCompatActivity {
     private CalendarView m_calendarView; //calendarView object
     private int m_year, m_month, m_dayOfMonth; //variables to store date
 
+    //called when activity is created
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
-        m_calendarView = (CalendarView) findViewById(R.id.calendarView);
-
-        Log.d("calendar", String.valueOf(m_calendarView.getDate()));
+        m_calendarView = (CalendarView) findViewById(R.id.calendarView); //initialize calendarView object
         m_calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) { //when date is selected
-                if (m_year == year && m_month == month && m_dayOfMonth == dayOfMonth) { //if same date is selected
+                if (m_year == year && m_month == month && m_dayOfMonth == dayOfMonth) { //if same date is selected go to journal activity
                     Intent intent = new Intent(CalendarActivity.this, JournalActivity.class);
                     intent.putExtra("year", m_year);
                     intent.putExtra("month", m_month);
                     intent.putExtra("dayOfMonth", m_dayOfMonth);
                     startActivity(intent);
-                } else {
+                } else { //if different date is selected
                     m_year = year;
                     m_month = month;
                     m_dayOfMonth = dayOfMonth;
--- code/MindScribe/app/src/main/java/com/example/mindscribe/JournalActivity.java
+++ code/MindScribe/app/src/main/java/com/example/mindscribe/JournalActivity.java
@@ -2,13 +2,22 @@ package com.example.mindscribe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class JournalActivity extends AppCompatActivity {
 
+    //called when activity is created
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journal);
+    }
+
+    //called when calendar button is clicked
+    public void calBtnOnClick(View v){
+        Intent intent = new Intent (JournalActivity.this, CalendarActivity.class);
+        startActivity(intent);
     }
 }
