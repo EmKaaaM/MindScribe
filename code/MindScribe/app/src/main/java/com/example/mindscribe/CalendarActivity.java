143d8b79e675dadeda0ab09b6db603a1e6e0297c
Louismaurice11 <louismaurice11@hotmail.com>
2023-11-22 13:10:32 +0000

    Add navigation from calendar to journal

--- code/MindScribe/app/src/main/java/com/example/mindscribe/CalendarActivity.java
+++ code/MindScribe/app/src/main/java/com/example/mindscribe/CalendarActivity.java
@@ -3,22 +3,39 @@ package com.example.mindscribe;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.CalendarView;
 
+import java.util.Calendar;
+
 public class CalendarActivity extends AppCompatActivity {
-    CalendarView calendarView;
+    private CalendarView m_calendarView; //calendarView object
+    private int m_year, m_month, m_dayOfMonth; //variables to store date
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
-        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        m_calendarView = (CalendarView) findViewById(R.id.calendarView);
 
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+        Log.d("calendar", String.valueOf(m_calendarView.getDate()));
+        m_calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                //unfinished method
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) { //when date is selected
+                if (m_year == year && m_month == month && m_dayOfMonth == dayOfMonth) { //if same date is selected
+                    Intent intent = new Intent(CalendarActivity.this, JournalActivity.class);
+                    intent.putExtra("year", m_year);
+                    intent.putExtra("month", m_month);
+                    intent.putExtra("dayOfMonth", m_dayOfMonth);
+                    startActivity(intent);
+                } else {
+                    m_year = year;
+                    m_month = month;
+                    m_dayOfMonth = dayOfMonth;
+                }
             }
         });
     }
