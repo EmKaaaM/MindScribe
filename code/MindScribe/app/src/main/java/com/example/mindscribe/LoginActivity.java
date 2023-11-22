65b6e3ba4aa62ff5d5f46076fcd5bf11f531c544
Louismaurice11 <louismaurice11@hotmail.com>
2023-11-22 16:26:36 +0000

    Fix minor error

--- code/MindScribe/app/src/main/java/com/example/mindscribe/LoginActivity.java
+++ code/MindScribe/app/src/main/java/com/example/mindscribe/LoginActivity.java
@@ -6,16 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+// TODO: Add login functionality
 public class LoginActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
 
-    public void calBtnOnClick(View v){
+    public void onLoginBtnClick(View v){
         Intent intent = new Intent (LoginActivity.this, MainActivity.class);
         startActivity(intent);
+        finish();
     }
 }
