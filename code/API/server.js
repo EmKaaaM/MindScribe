38a170677324b76ef1c66972de2146e139c3a428
Xyntlock <70027309+Xyntlock@users.noreply.github.com>
2023-11-25 13:46:06 +0000

    21 Enable Account creation and login via REST API (#31)
    * add login functionality
    
    * add account creation functionality
    
    * Login from app
    
    * Minor aeshetic changes to login activity
    
    ---------
    
    Co-authored-by: Louismaurice11 <louismaurice11@hotmail.com>

--- code/API/server.js
+++ code/API/server.js
@@ -3,7 +3,10 @@ import http from 'http'
 import 'dotenv/config'
 import pkg from 'pg'
 import { createTerminus } from '@godaddy/terminus'
+import bcrypt from 'bcrypt'
 const { Pool } = pkg
+
+const saltRounds = 12;
 
 // pool of clients to connect to DB
 const pool = new Pool({
@@ -11,6 +14,7 @@ const pool = new Pool({
 })
 
 const app = express()
+app.use(express.json())
 
 // User endpoint
 app.get('/users/:id', async (req, res) => {
@@ -21,6 +25,54 @@ app.get('/users/:id', async (req, res) => {
     const user = rows.filter(user => Number(user.user_id) === id)
 
     res.send(user)
+})
+
+app.post('/login', async (req, res) => {
+    await pool.connect()
+
+    console.log('request made')
+    console.log(req.params.username, req.body.username)
+
+    const username = req.body.username
+    const password = req.body.password
+
+    const { rows } = await pool.query(`SELECT * FROM users WHERE username = '${username}'`);
+    if (rows.length === 0) {
+        res.status(401).send({body: 'Invalid Credentials'})
+        return
+    }
+
+    const dbPasswordHash = rows[0].password_hash
+    const dbPass = bcrypt.compareSync(password, dbPasswordHash)
+
+    if (dbPass) {
+        res.status(200).send({body: 'Login Succeeded'})
+    }
+    else {
+        res.status(401).send({body: 'Invalid Credentials'})
+    }
+})
+
+app.post('/createAccount', async (req, res) => {
+    await pool.connect()
+
+    const username = req.body.username
+    const password = req.body.password
+
+    const { rows } = await pool.query(`SELECT * FROM users WHERE username = '${username}'`)
+    if (rows.length !== 0) {
+        res.status(400).send({body: 'Username already exists'})
+    }
+
+    bcrypt.hash(password, saltRounds, async function (err, hash) {
+        if (err) {
+            res.status(400).send({body: 'Error occured during creation'})
+            return
+        }
+
+        await pool.query(`INSERT INTO users (username, password_hash) VALUES ('${username}', '${hash}')`)
+        res.status(200).send({body: 'Account created'})
+    })
 })
 
 const server = http.createServer(app)
--- code/MindScribe/app/src/main/java/com/example/mindscribe/LoginActivity.java
+++ code/MindScribe/app/src/main/java/com/example/mindscribe/LoginActivity.java
@@ -4,9 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
-// TODO: Add login functionality
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +30,31 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
-    public void onLoginBtnClick(View v){
-        Intent intent = new Intent (LoginActivity.this, HomeActivity.class);
-        startActivity(intent);
-        finish();
+    public void onLoginBtnClick(View v) throws JSONException {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String url = "http://10.0.2.2:3001/login";
+
+        EditText usernameTextView = findViewById(R.id.usernameText);
+        EditText passwordView = findViewById(R.id.passwordText);
+
+        HashMap<String,String> params = new HashMap<>();
+        params.put("username", usernameTextView.getText().toString());
+        params.put("password", passwordView.getText().toString());
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(params),
+                response -> {
+                    Log.d("COMP32", response.toString());
+                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                }, error -> {
+                    Log.d("COMP32", error.toString());
+                    passwordView.setText("");
+                    usernameTextView.setText("");
+                    usernameTextView.setHint("Incorrect");
+                }){
+        };
+
+        requestQueue.add(request);
     }
 }
