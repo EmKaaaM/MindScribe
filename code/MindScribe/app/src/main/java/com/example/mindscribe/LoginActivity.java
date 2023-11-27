f92317f736479c8c8316de9c6b0e6f35389038e8
Xyntlock <jacobjohn1005@gmail.com>
2023-11-27 14:41:28 +0000

    Remove logs and unused imports

--- code/MindScribe/app/src/main/java/com/example/mindscribe/LoginActivity.java
+++ code/MindScribe/app/src/main/java/com/example/mindscribe/LoginActivity.java
@@ -4,24 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
     @Override
@@ -43,17 +37,15 @@ public class LoginActivity extends AppCompatActivity {
 
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(params),
                 response -> {
-                    Log.d("COMP32", response.toString());
                     Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
                     startActivity(intent);
                     finish();
                 }, error -> {
-                    Log.d("COMP32", error.toString());
                     passwordView.setText("");
                     usernameTextView.setText("");
                     usernameTextView.setHint("Incorrect");
-                }){
-        };
+                }
+        );
 
         requestQueue.add(request);
     }
