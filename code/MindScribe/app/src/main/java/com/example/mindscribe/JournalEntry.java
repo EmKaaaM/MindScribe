2782f4cbe0cb937af8831324dab55af2cd62ce5a
EmKaaaM <emilijus.miniauskas@gmail.com>
2023-11-27 18:33:04 +0000

    Add JWT auth, add API retrieve and send
    Added JWT authentication, added API retrieval of entries and sending to the database, added a entry_created. Ensured that it wouldn't add duplicate entries. Made sure that entries are displayed and cached on the app.

--- code/MindScribe/app/src/main/java/com/example/mindscribe/JournalEntry.java
+++ code/MindScribe/app/src/main/java/com/example/mindscribe/JournalEntry.java
@@ -1,15 +1,22 @@
 package com.example.mindscribe;
-
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.json.JSONObject;
 public class JournalEntry {
     private int m_year, m_month, m_dayOfMonth; //variables to store date
     String entry; //variable to store entry
+    private int userID;
 
     //constructor
-    public JournalEntry(int year, int month, int dayOfMonth, String entry){
+    public JournalEntry(int year, int month, int dayOfMonth, String entry, int userId) {
         m_year = year;
         m_month = month;
         m_dayOfMonth = dayOfMonth;
         this.entry = entry;
+        this.userID = userId;
     }
 
     //getters
@@ -45,4 +52,13 @@ public class JournalEntry {
     public void setEntry(String entry){
         this.entry = entry;
     }
+
+    public void setUserID(int userID){
+        this.userID = userID;
+    }
+
+    public int getUserID(){
+        return userID;
+    }
+
 }
