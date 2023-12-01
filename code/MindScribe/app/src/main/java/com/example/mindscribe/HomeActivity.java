2f5ece20f1a351bbb1d7c293d034bf17d1e563b0
Carl <72538706+CarlG17@users.noreply.github.com>
2023-12-01 16:43:02 +0000

    Button added to edit today's journal
    - Button added on homepage to take user straight to today's journal, unable to test at the moment due to API issue on my end
    - Still need to make sure everything flows properly, then add today's journal on homepage to view

--- code/MindScribe/app/src/main/java/com/example/mindscribe/HomeActivity.java
+++ code/MindScribe/app/src/main/java/com/example/mindscribe/HomeActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.Calendar;
+
 public class HomeActivity extends AppCompatActivity {
 
     //called when activity is created
@@ -16,12 +18,27 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     //called when calendar button is clicked
-    public void onCalBtnClick(View v){
-        Intent intent = new Intent (HomeActivity.this, CalendarActivity.class);
+    public void onCalBtnClick(View v) {
+        Intent intent = new Intent(HomeActivity.this, CalendarActivity.class);
         startActivity(intent);
     }
 
-    public void onHomeBtnClick(View v){
+    public void onHomeBtnClick(View v) {
         //do nothing
+    }
+
+    public void onTodayBtnClick(View v) {
+        Calendar currentDate = Calendar.getInstance();
+        // Fetch the current date to pass to the Journal activity
+        int year = currentDate.get(Calendar.YEAR);
+        int month = currentDate.get(Calendar.MONTH);
+        int day = currentDate.get(Calendar.DAY_OF_MONTH);
+
+        // Package variables to send to Journal activity, and open it
+        Intent intent = new Intent(HomeActivity.this, JournalActivity.class);
+        intent.putExtra("year", year);
+        intent.putExtra("month", month);
+        intent.putExtra("dayOfMonth", day);
+        startActivity(intent);
     }
 }
